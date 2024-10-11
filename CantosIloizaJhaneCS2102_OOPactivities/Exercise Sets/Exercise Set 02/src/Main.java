d0c82e84c8e93ee947e5ec7116f62e8236ee5772
Iloiza Jhane <163818149+cantos-iloiza@users.noreply.github.com>
2024-10-11 16:17:03 +0800

    Update Main.java

--- CantosIloizaJhaneCS2102_OOPactivities/Exercise Sets/Exercise Set 02/src/Main.java
+++ CantosIloizaJhaneCS2102_OOPactivities/Exercise Sets/Exercise Set 02/src/Main.java
@@ -22,3 +22,16 @@ public class Main {
     }
 
 }
+
+/*Example Strings to Input:
+1. For a red car priced at 19999.85 and of medium size: 
+Color of the car: red
+Price of the car: 19999.85
+Size of the car: s
+Output: Car (red) - P19999.85 - medium
+
+2. For a blue car priced at 50000.00 and large: 
+Color of the car: blue
+Price of the car: 50000
+Size of the car: l
+Output: Car (blue) - P50000.00 - large*/
