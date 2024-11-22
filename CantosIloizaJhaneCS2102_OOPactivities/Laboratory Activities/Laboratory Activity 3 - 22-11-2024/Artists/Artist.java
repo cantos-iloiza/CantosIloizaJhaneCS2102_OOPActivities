37bf786fb77fd66236db3d9aa357b69abb52d958
Iloiza Jhane <163818149+cantos-iloiza@users.noreply.github.com>
2024-11-22 16:31:01 +0800

    Update Artist.java

--- CantosIloizaJhaneCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/Artist.java
+++ CantosIloizaJhaneCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/Artist.java
@@ -1,23 +1,17 @@
 public class Artist {
 
     private String name;
-    private String nationality;
     private int age;
     private String specialty;
     
-    public Artist(String name, String nationality, int age, String specialty) {
+    public Artist(String name, int age, String specialty) {
         this.name = name;
-        this.nationality = nationality;
         this.age = age;
         this.specialty = specialty;
     }
 
     public String getName() {
         return name;
-    }
-    
-    public String getNationality() {
-        return nationality;
     }
 
     public int getAge() {
@@ -30,10 +24,6 @@ public class Artist {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
     }
 
     public void setAge(int age) {
