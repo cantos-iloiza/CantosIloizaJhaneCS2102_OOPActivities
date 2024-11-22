ab43fef349db703c062170672647357cce936dfe
Iloiza Jhane <163818149+cantos-iloiza@users.noreply.github.com>
2024-11-22 17:46:16 +0800

    Update Singer.java

--- CantosIloizaJhaneCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/Singer.java
+++ CantosIloizaJhaneCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/Singer.java
@@ -1,33 +1,30 @@
-enum Genre {
-    POP,
-    ROCK,
-    CLASSICAL,
-    JAZZ,
-    HIPHOP,
-    RNB
+enum WritingStyle {
+    FICTION,
+    NONFICTION,
+    POETRY,
+    DRAMA
 }
 
-class Singer extends Artist {
-    private Genre genre;
+class Writer extends Artist {
+    private WritingStyle writingStyle;
 
-    public Singer(String name, String nationality, int age, String specialty, Genre genre) {
+    public Writer(String name, String nationality, int age, String specialty, WritingStyle writingStyle) {
         super(name, nationality, age, specialty);
-        this.genre = genre;
+        this.writingStyle = writingStyle;
     }
 
-    public Genre getGenre() {
-        return genre;
+    public WritingStyle getWritingStyle(){
+        return writingStyle;
     }
 
-    public void setGenre(Genre genre) {
-        this.genre = genre;
+    public void setWritingStyle(WritingStyle writingStyle) {
+        this.writingStyle = writingStyle;
     }
 
     @Override
     public void displayInfo() {
-        System.out.println("== Singer Info ==");
+        System.out.println("== Writer Info ==");
         super.displayInfo();
-        System.out.println("Genre: " + genre);
+        System.out.println("Writing Style: " + writingStyle);
     }
 }
-
