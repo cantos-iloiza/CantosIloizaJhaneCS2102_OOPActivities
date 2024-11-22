948afdb98fb77ec8d0fe2be6eb01c1e84bf481be
Iloiza Jhane <163818149+cantos-iloiza@users.noreply.github.com>
2024-11-22 17:45:03 +0800

    Update ArtistDemo.java

--- CantosIloizaJhaneCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/ArtistDemo.java
+++ CantosIloizaJhaneCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/ArtistDemo.java
@@ -1,26 +1,26 @@
 public class ArtistDemo {
     public static void main(String[] args) {
-        Artist artist = new Artist("Charlie Puth", 32, "Music");
+        Artist artist = new Artist("Britney Spears", "American", 42, "Music");
         artist.displayInfo();
 
         System.out.println();
 
-        Singer singer = new Singer("Nayeon Im", 29, "Music", Genre.POP);
+        Singer singer = new Singer("Kenshi Yonezu", "Japanese", 33, "Music", Genre.POP);
         singer.displayInfo();
 
         System.out.println();
 
-        Painter painter = new Painter("Kehinde Wiley", 47, "Art", Medium.OIL);
+        Painter painter = new Painter("Kehinde Wiley", "American", 47, "Art", Medium.OIL);
         painter.displayInfo();
 
         System.out.println();
 
-        Writer writer = new Writer("Joanne 'J. K.' Rowling", 59, "Literature", WritingStyle.FICTION);
+        Writer writer = new Writer("Joanne 'J. K.' Rowling", "British", 59, "Literature", WritingStyle.FICTION);
         writer.displayInfo();
 
         System.out.println();
 
-        Dancer dancer = new Dancer("Audrey Lane", 20, "Dance", DanceStyle.HIPHOP);
+        Dancer dancer = new Dancer("Audrey Lane", "American", 20, "Dance", DanceStyle.HIPHOP);
         dancer.displayInfo();
     }
 }
